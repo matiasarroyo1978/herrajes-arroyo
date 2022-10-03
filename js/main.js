51fd6150226d158ead0434a806675e2e3ea39d5b
Matias Arroyo <matiasarroyo684@gmail.com>
2022-10-03 15:36:59 -0300

    proyecto final

--- js/main.js
+++ js/main.js
@@ -37,24 +37,26 @@
 
         return check;
     });
-
-
+     
     $('.validate-form .input1').each(function(){
         $(this).focus(function(){
            hideValidate(this);
        });
+       
     });
 
     function showValidate(input) {
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
+        
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
 
         $(thisAlert).removeClass('alert-validate');
+        
     }
     
     
